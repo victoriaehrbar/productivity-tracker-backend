c0277a15d0311b03bef590e38964ded55955c853
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-02-28 12:04:45 -0600

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :habits
+      resources :habits do
         resources :months
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     end
   end
+end
 
 end
